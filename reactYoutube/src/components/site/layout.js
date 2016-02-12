16a6130f8cc76c084361ed17037be660598bb409
Brian Christensen <brian.christensen@hart.com>
2016-02-12 14:15:27 -0800

    Cleaned up layout

--- reactYoutube/src/components/site/layout.js
+++ reactYoutube/src/components/site/layout.js
@@ -1,14 +1,6 @@
 import React, { PropTypes } from 'react';
 
-// Note: Stateless/function components *will not* hot reload!
-// react-transform *only* works on component classes.
-//
-// Since layouts rarely change, they are a good place to
-// leverage React's new Stateless Functions:
-// https://facebook.github.io/react/docs/reusable-components.html#stateless-functions
-//
-// Layout is a pure function of its props, so we can
-// define it with a plain javascript function...
+// stateless function
 function Layout ({ children }) {
 	return (
 		<div className='container-fluid'>
